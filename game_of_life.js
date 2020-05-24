1418c349c59a005a17ab8513a037e09b5b1751f1
Alexander <ancorwin@gmail.com>
2020-05-24 13:09:38 -0700

    adding a game of life sim
    to have something to look at when i dont use my webcam

---
+++ game_of_life.js
@@ -0,0 +1,386 @@
+"use strict";
+
+function strToBoard(s) {
+	var board = [];
+	for (var row of s.trim().split('\n')) {
+		board.push(row.trim().split('').map(el => el === '0' ? false : true));
+	}
+	return board;
+}
+
+let gosper_glider_gun = `
+000000000000000000000000100000000000
+000000000000000000000010100000000000
+000000000000110000001100000000000011
+000000000001000100001100000000000011
+110000000010000010001100000000000000
+110000000010001011000010100000000000
+000000000010000010000000100000000000
+000000000001000100000000000000000000
+000000000000110000000000000000000000`;
+
+let pattern_strings = [
+`010
+ 001
+ 111`,
+
+`010
+ 111
+ 101
+ 010`,
+
+`10101
+ 10001
+ 10001
+ 10001
+ 10101`,
+
+`1111111111`,
+
+`01111
+ 10001
+ 00001
+ 10010`,
+
+`0110110
+ 0110110
+ 0010100
+ 1010101
+ 1010101
+ 1100011`,
+
+ gosper_glider_gun,
+
+`000000110000
+ 000000110000
+ 000000000000
+ 000011110000
+ 110100101000
+ 110101001000
+ 000100011011
+ 000100001011
+ 000011110000
+ 000000000000
+ 000011000000
+ 000011000000`,
+
+]
+
+let PATTERNS = pattern_strings.map(strToBoard);
+const HISTORY_LENGTH = 300;
+
+function History(length) {
+	// we're just initializing the history to non-equal values so
+	// it will always lie about whether or not the history
+	// is uniform until you've pushed at least `length`
+	// elements on to it.
+	// i don't care, but it is technically incorrect.
+	var history = new Array(length),
+	    index = 0,
+	    full = false;
+
+	this.push = function(el) {
+		history[index] = el;
+		index = (index + 1) % length;
+		if (index === 0) {
+			full = true;
+		}
+	}
+
+	function inserted() {
+		if (full) {
+			return history;
+		} else {
+			return history.slice(0, index);
+		}
+
+	}
+
+	this.isFull = function() {
+		return full;
+	}
+
+	this.allEqual = function() {
+		var s = history[0];
+		for (var el of inserted()) {
+			if (el !== s) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	this.uniques = function() {
+		return new Set(inserted()).size;
+	}
+
+	this.reset = function() {
+		for (var i = 0; i < length; i++) {
+			// initialize with non-equal values
+			history[i] = 0;
+		}
+		full = false;
+		index = 0;
+	}
+	this.reset();
+}
+
+function GameOfLife(config) {
+	let width = config && config.width || 30;
+	let height = config && config.height || 30;
+	// it would be reasonable to configure these as a 
+	// full state machine description, if i cared
+	let to_die = new Set(config && config.to_die || [0, 1, 4, 5, 6, 7, 8]);
+	let to_grow = new Set(config && config.to_grow || [3]);
+	let ticks_after_stuck = (config && config.ticks_after_stuck || 13);
+	var board = null,
+	    ticks_stuck = 0,
+	    history = new Set(),
+	    uniques_last_tick = 10000; // big number, otherwise meaningless
+
+
+	this.getWidth = function() {
+		return width;
+	}
+
+	this.getHeight = function() {
+		return height;
+	}
+
+	this.zeroBoard = function(_rows, _cols) {
+		var rows = _rows || height,
+		    cols = _cols || width;
+		var newBoard = [];
+		for (var row = 0; row < rows; row++) {
+			var newRow = [];
+			for (var col = 0; col < cols; col++) {
+				newRow.push(false);
+			}
+			newBoard.push(newRow);
+		}
+		this.setBoard(newBoard);
+	}
+
+	this.setBoard = function(_board) {
+		assert(_board !== null && _board !== undefined, 'setBoard() passed null board');
+		assert(_board.length == height, 'setBoard() passed board of invalid height');
+		assert(_board[0].length == width, 'setBoard() passed board of invalid height');
+		history.clear();
+		board = _board;
+		ticks_stuck = 0;
+		uniques_last_tick = 10000; // big number, otherwise meaningless
+	}
+
+	function copy2DMatrix(matrix) {
+		var copy = [];
+		for (var row of matrix) {
+			var new_row = [];
+			for (var el of row) {
+				new_row.push(el);
+			}
+			copy.push(new_row);
+		}
+		return copy;
+
+	}
+
+	this.copyBoard = function() {
+		return copy2DMatrix(board);
+	}
+
+	this.viewBoard = function() {
+	}
+
+	this.newRandomBoard = function(alive_weight) {
+		assert(0 < alive_weight && alive_weight < 1);
+		var newBoard = [];
+		for (var i = 0; i < height; i++) {
+			var row = [];
+			for (var j = 0; j < width; j++) {
+				let alive = Math.random() < alive_weight;
+				row.push(alive);
+			}
+			newBoard.push(row);
+		}
+		this.setBoard(newBoard);
+	}
+
+
+	this.newBoard = function(options) {
+		// what am i thinking here? why did i add this indirection?
+		if (!options) {
+			this.newRandomBoard(0.5);
+		}
+	}
+
+	this.toString = function(a_board) {
+		return matrixToString(
+			a_board,
+			el => el ? '\u25A0' : '\u25A1'
+		);
+	}
+	
+	this.tick = function() {
+		// this fucking sucks but it's correct i guess
+		// keeping it as a local func here b/c the faster algorithm involves scanning
+		// and is thus *actually* local in a way this isn't.
+		function neighbours(row_num, col_num) {
+			// row_num
+			assert(row_num >= 0 && row_num < height, 'Invalid row num');
+			assert(col_num >= 0 && col_num < width,  'Invalid col num');
+			var n = 0;
+			var prev_row = board[row_num-1];
+			var next_row = board[row_num+1];
+			if (prev_row !== undefined) {
+				for (var offset of [-1, 0, 1]) {
+					if (prev_row[col_num + offset] === true) {
+						n += 1;
+					}
+				}
+			}
+			if (next_row !== undefined) {
+				for (var offset of [-1, 0, 1]) {
+					if (next_row[col_num + offset] === true) {
+						n += 1;
+					}
+				}
+			}
+			if (board[row_num][col_num-1] === true) {
+				n += 1;
+			}
+			if (board[row_num][col_num+1] === true) {
+				n += 1;
+			}
+			assert(n < 9, 'too many neighbours');
+			return n;
+		}
+
+		var next_gen = []
+		var neighbour_counts = [];
+		for (var row = 0; row < height; row++) {
+			var new_row = [];
+			var next_gen_row = []
+			for (var col = 0; col < width; col++) {
+				let n_neighbours = neighbours(row, col);
+				new_row.push(n_neighbours);
+				if (board[row][col]) {
+					let survives = !to_die.has(n_neighbours);
+					next_gen_row.push(survives);
+				} else {
+					let grows = to_grow.has(n_neighbours)
+					next_gen_row.push(grows);
+				}
+			}
+			neighbour_counts.push(new_row);
+			next_gen.push(next_gen_row);
+		}
+		board = next_gen;
+		history.add(hash());
+		let uniques = history.size;
+		if (uniques === uniques_last_tick) {
+			if (ticks_stuck > 0) {
+				ticks_stuck += 1;
+			} else {
+				ticks_stuck = 1;
+			}
+		} else {
+			ticks_stuck = 0;
+		}
+		uniques_last_tick = uniques;
+		return board;
+	}
+
+	this.setRect = function(sub_rect, _start_col, _start_row) {
+		assert(!!board, 'board logically false??');
+		let start_row = _start_row || 0,
+		    start_col = _start_col || 0;
+		let s_height = sub_rect.length;
+		let s_width = sub_rect[0].length;
+		let setting_top = s_height + start_row;
+		let setting_left = s_width + start_col;
+		assert(start_row < height, 'starting beyond vertical borders');
+		assert(start_col < width,  'starting beyond horizontal borders');
+
+		assert(height >= start_row + sub_rect.length,    'too many rows');
+		assert(width >=  sub_rect[0].length,        'too many cols');
+
+		assert(height - (s_height + start_row) >= 0, 'invalid subRect() height ' + s_height);
+		assert(width  - (s_width + start_col) >= 0, 'invalid subRect() width ' + s_width);
+		for (var row_n = 0; row_n < sub_rect.length; row_n++) {
+			let this_row = board[row_n + start_row];
+			if (!this_row) {
+				debugger;
+			}
+			for (var col_n = 0; col_n < sub_rect[0].length; col_n++) {
+				let liveness = sub_rect[row_n][col_n];
+				this_row[col_n + start_col] = liveness;
+			}
+		}
+		
+	}
+
+	function hash() {
+		/*
+		assert(width <= 64, 'too wide to hash like this');
+		var hash = 17;
+		for(var row of board){
+			for (var cell of row) {
+				let inty = cell ? 1 : 0;
+				hash = (31 * hash + inty) % Number.MAX_SAFE_INTEGER;
+			}
+		}
+		return hash;	
+		*/
+		var str = [];
+		for(var row of board){
+			for (var cell of row) {
+				str.push(cell ? '1' : '0');
+			}
+		}
+		return str.join('');
+	}
+
+	this.isStale = function() {
+		return ticks_stuck >= ticks_after_stuck;
+	}
+}
+
+function matrixToString(matrix, transform) {
+	let thing = matrix.map(
+		function(row) {
+			return row.map(
+				transform
+			).join(' ');
+		}).join('\n');
+	return thing;
+}
+
+function flexBoxInitial(height, width) {
+	var container = document.createElement('div');
+	container.classList.add('gol_container');
+	var els_matrix = [];
+	for (var row_n = 0; row_n < height; row_n++) {
+		var row_el = document.createElement('div');
+		row_el.classList.add('row');
+		var col_n = 0;
+		var els_row = [];
+		for (var col_n = 0; col_n < width; col_n++) {
+			var cell = document.createElement('div');
+			cell.classList.add('cell');
+			cell.setAttribute('id', 'cell_' + row_n + '_' + col_n);
+			row_el.appendChild(cell);
+			els_row.push(cell);
+		}
+		container.appendChild(row_el);
+		els_matrix.push(els_row);
+	}
+	return {
+		container: container,
+		els: els_matrix,
+	};
+
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+});
--- layout.js
+++ layout.js
@@ -1,121 +1,130 @@
 "use strict";
-
 function assert(val, msg) {
 	if (!val) {
-		alert(msg);
+		//debugger;
 	}
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+function randomInt(max) {
+	return Math.floor(Math.random() * Math.floor(max));
+}
 
-	function randomInt(max) {
-		return Math.floor(Math.random() * Math.floor(max));
+function randomScaledFloat(min, max) {
+    return (Math.random() * (max - min)) + min;
+
+}
+
+function randomScaledInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomRGBComponent() {
+	return randomInt(255);
+}
+
+function randomRGBNumbers() {
+	return [randomInt(255), randomInt(255), randomInt(255)];
+}
+
+function RGBToStyleString(rgb) {
+	return 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
+}
+
+function randomRGBString() {
+	return RGBToStyleString(randomRGBNumbers());
+}
+
+function ChangingElement(_element, _ticks) {
+	let element = _element,
+	    ticks_target = _ticks || 60;
+	var current = [0, 0, 0],
+	    target = randomRGBNumbers(),
+	    ticks = 0,
+	    wait_ticks = 0,
+	    state = 'waiting',
+	    wait_ticks_target,
+	    deltas;
+
+	function resetTickDeltas() {
+		var ldeltas = [];
+		for (var i of [0, 1, 2]) {
+			ldeltas[i] = (target[i] - current[i]) / ticks_target;
+		}
+		deltas = ldeltas;
 	}
 
-	function randomRGBComponent() {
-		return randomInt(255);
+	this.tick = function() {
+		assert(state == 'ticking', 'invalid state ' + state + ' in tick()');
+		for (var i of [0, 1, 2]) {
+			current[i] += deltas[i];
+		}
+		ticks += 1;
+		element.style.color = RGBToStyleString(current);
+	}
+
+	this.isDoneTicking = function() {
+		assert(state == 'ticking', 'invalid state ' + state + ' in isDoneTicking()');
+		return (
+			Math.abs(current[0] - target[0]) < 0.1 &&
+			Math.abs(current[1] - target[1]) < 0.1 &&
+			Math.abs(current[2] - target[2]) < 0.1
+		) || ticks == ticks_target;
+	}
+
+	this.startWait = function(_wait_ticks) {
+		assert(state == 'ticking', 'invalid state ' + state + ' in startWait()');
+		state = 'waiting';
+		wait_ticks_target = _wait_ticks || randomInt(30);
+		wait_ticks = 0;
+		//console.log('Starting to wait for ' + wait_ticks_target + ' ticks');
+	}
+
+	this.isDoneWaiting = function() {
+		assert(state == 'waiting', 'invalid state ' + state + ' in isDoneWaiting()');
+		return wait_ticks >= wait_ticks_target;
 	}
 	
-	function randomRGBNumbers() {
-		return [randomInt(255), randomInt(255), randomInt(255)];
+	this.tickWait = function() {
+		assert(state == 'waiting', 'invalid state ' + state + ' in tickWait()');
+		wait_ticks += 1;
+		//console.log('Waited again; wait_ticks now at ' + wait_ticks);
+	}
+	
+	this.resetTarget = function(rgb) {
+		assert(state == 'waiting', 'invalid state ' + state + ' in resetTarget()');
+		ticks = 0;
+		target = rgb || randomRGBNumbers();
+		state = 'ticking';
+		resetTickDeltas();
+		//console.log('resetting target');
 	}
 
-	function RGBToStyleString(rgb) {
-		return 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
-	}
-
-	function randomRGBString() {
-		return RGBToStyleString(randomRGBNumbers());
-	}
-
-	function ChangingElement(_element, _current, _target, _ticks) {
-		let element = _element,
-		    ticks_target = _ticks || 60;
-		var current = _current || _element.style.color || [0, 0, 0],
-		    target = _target || randomRGBNumbers(),
-		    ticks = 0,
-		    wait_ticks = 0,
-		    state = 'waiting',
-		    wait_ticks_target,
-		    deltas;
-
-		function resetTickDeltas() {
-			var ldeltas = [];
-			for (var i of [0, 1, 2]) {
-				ldeltas[i] = (target[i] - current[i]) / ticks_target;
-			}
-			deltas = ldeltas;
-		}
-
-		this.tick = function() {
-			assert(state == 'ticking', 'invalid state ' + state + ' in tick()');
-			for (var i of [0, 1, 2]) {
-				current[i] += deltas[i];
-			}
-			ticks += 1;
-			element.style.color = RGBToStyleString(current);
-		}
-
-		this.isDoneTicking = function() {
-			assert(state == 'ticking', 'invalid state ' + state + ' in isDoneTicking()');
-			return (
-				Math.abs(current[0] - target[0]) < 0.1 &&
-				Math.abs(current[1] - target[1]) < 0.1 &&
-				Math.abs(current[2] - target[2]) < 0.1
-			) || ticks == ticks_target;
-		}
-
-		this.startWait = function(_wait_ticks) {
-			assert(state == 'ticking', 'invalid state ' + state + ' in startWait()');
-			state = 'waiting';
-			wait_ticks_target = _wait_ticks || randomInt(30);
-			wait_ticks = 0;
-			console.log('Starting to wait for ' + wait_ticks_target + ' ticks');
-		}
-
-		this.isDoneWaiting = function() {
-			assert(state == 'waiting', 'invalid state ' + state + ' in isDoneWaiting()');
-			return wait_ticks >= wait_ticks_target;
-		}
-		
-		this.tickWait = function() {
-			assert(state == 'waiting', 'invalid state ' + state + ' in tickWait()');
-			wait_ticks += 1;
-			console.log('Waited again; wait_ticks now at ' + wait_ticks);
-		}
-		
-		this.resetTarget = function(rgb) {
-			assert(state == 'waiting', 'invalid state ' + state + ' in resetTarget()');
-			ticks = 0;
-			target = rgb || randomRGBNumbers();
-			state = 'ticking';
-			resetTickDeltas();
-			console.log('resetting target');
-		}
-
-		this.pump = function() {
-			if (state === 'ticking') {
-				if (this.isDoneTicking()) {
-					this.startWait();
-				} else {
-					this.tick();
-				}
-			} else if (state === 'waiting') {
-				if (this.isDoneWaiting()) {
-					this.resetTarget();
-				} else {
-					this.tickWait();
-				}
+	this.pump = function() {
+		if (state === 'ticking') {
+			if (this.isDoneTicking()) {
+				this.startWait();
 			} else {
-				assert(false, 'invalid state ' + state + ' in pump()');
+				this.tick();
 			}
-
+		} else if (state === 'waiting') {
+			if (this.isDoneWaiting()) {
+				this.resetTarget();
+			} else {
+				this.tickWait();
+			}
+		} else {
+			assert(false, 'invalid state ' + state + ' in pump()');
 		}
-
-		this.resetTarget();
 
 	}
 
+	this.resetTarget();
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
 
 	let f = document.getElementById('F'),
 	    o = document.getElementById('O'),
@@ -125,25 +134,88 @@ document.addEventListener('DOMContentLoaded', function() {
 	    s = document.getElementById('S'),
 	    k = document.getElementById('K'),
 	    letters = [
-		    new ChangingElement(f),
-		    new ChangingElement(o),
-		    new ChangingElement(x),
-		    new ChangingElement(l),
-		    new ChangingElement(i),
-		    new ChangingElement(s),
-		    new ChangingElement(k),
-	    ];
+		    new ChangingElement(f, 240),
+		    new ChangingElement(o, 240),
+		    new ChangingElement(x, 240),
+		    new ChangingElement(l, 240),
+		    new ChangingElement(i, 240),
+		    new ChangingElement(s, 240),
+		    new ChangingElement(k, 240),
+	    ],
+	    gol_el = document.getElementById('right_box_2'),
+	    starting_gol_html_elements,
+	    static_ticks_before_starting = 20,
+	    static_ticks = 0;
 
-         
-	    //letters = [f, o, x, l, i, s, k];
-
-
-	var intervalId;
 	function tickLetters() {
 		for (var letter of letters) {
 			letter.pump();
 		}
 	}
-	intervalId = setInterval(tickLetters, 20);
+	setInterval(tickLetters, 20);
+
+
+	let height = 40, width = 50;
+
+	var gol = new GameOfLife({
+		height: height,
+		width: width,
+		ticks_after_stuck: 20,
+	});
+	gol.zeroBoard();
+	starting_gol_html_elements = flexBoxInitial(height, width);
+	gol_el.innerHTML = '';
+	gol_el.append(starting_gol_html_elements.container);
+
+	function resetBoard(gol) {
+		gol.zeroBoard();
+
+		if (Math.random() < 0.4) {
+			//let pattern = strToBoard(gosper_glider_gun);
+			//let startx = 4;
+			//let starty = 4;
+			let pattern = PATTERNS[randomInt(PATTERNS.length)];
+			let startx = randomInt(gol.getWidth() - pattern[0].length);
+			let starty = randomInt(gol.getHeight() - pattern.length);
+			gol.setRect(pattern, startx, starty);
+		} else {
+			gol.newRandomBoard(randomScaledFloat(0.3, 0.7));
+		}
+			
+
+	}
+	function drawBoard(new_board) {
+		for (var row_n in new_board) {
+			for (var col_n in new_board[row_n]) {
+				if (!starting_gol_html_elements.els[row_n][col_n]) {
+					//lool
+				}
+				starting_gol_html_elements.els[row_n][col_n].classList.remove('alive')
+				if (new_board[row_n][col_n]) {
+					starting_gol_html_elements.els[row_n][col_n].classList.add('alive');
+				}
+			}
+		}
+	}
+
+	resetBoard(gol);
+	drawBoard(gol.copyBoard());
+
+	function run_the_gol() {
+		if (gol.isStale()) {
+			resetBoard(gol);
+			static_ticks = 0;
+			drawBoard(gol.copyBoard());
+		}
+		if (static_ticks < static_ticks_before_starting) {
+			static_ticks += 1;
+			return;
+		}
+		var new_board = gol.tick();
+		drawBoard(new_board);
+
+	}
+
+	setInterval(run_the_gol, 70);
 
 });
